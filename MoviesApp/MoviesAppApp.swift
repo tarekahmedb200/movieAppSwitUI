bac2aef65c0bc5d745805d8d2917e2112c254c3f
tarekahmed199 <tarek.ahmed@nandbox.com>
2023-06-24 15:24:35 +0300

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by lapshop on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
